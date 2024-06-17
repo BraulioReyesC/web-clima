e351446ba977052f997b2914d1ca3226799b24ee
PaulReyC <139603247+PaulReyC@users.noreply.github.com>
2024-06-16 21:51:10 -0400

    Adicion vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-  ,devServer: {
+  transpileDependencies: true,
+  devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/nombredelrepositorio/'  // Reemplaza 'nombredelrepositorio' con el nombre de tu repositorio
+    : '/'
 })
